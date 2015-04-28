d40fea7ef25cfdbaacc7825c99993e66f3f3a828
mokpro <pranav.moktali@gmail.com>
2015-04-27 23:50:25 -0500

    cleaning the tweet text

--- putDB.py
+++ putDB.py
@@ -13,29 +13,40 @@ def connect():
 def write_to_csv():
 	infile = open("data.json","r")
 	data = json.load(infile)
+	count = 0
 	non_eng_count = 0
 	q_count = 0
 	with open('data.csv', 'wb') as csvfile:
 		tweet_writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		tweet_writer.writerow(['tweet_id', 'tweet_text', 'language','is_question'])
+		tweet_writer.writerow(['tweet_id', 'tweet_original','tweet_clean','language','has_question_word','question_word'])
 		for item in data:
-			is_question = ''
+			tweet_clean = clean_the_tweet_text(item["tweet"])
+			has_question_word = ''
+			question_word = ''
+			question_words = ['?', 'when', 'why', 'where', 'which', 'how', 'what', 'who']
+
 			if item["language"] == "en":
-				if re.search('\?',item["tweet"]):
-					is_question = 'yes'
-					q_count +=1 
+				for q_word in question_words:
+					if  q_word in item["tweet"].lower().split(' '):
+						has_question_word = 'yes'
+						question_word = q_word
+						q_count+=1
 				try:
-					tweet_writer.writerow([str(item["tweet_id"]), item["tweet"].encode("utf-8"), str(item["language"]),is_question])
+					tweet_writer.writerow([str(item["tweet_id"]), item["tweet"].encode("utf-8"),tweet_clean, str(item["language"]),has_question_word, question_word])
 				except Exception, e:
 					print item
 					# raise e
 					count+=1
 			else:
 				non_eng_count+=1
-		print "non_eng_count:",non_eng_count
+		print "count:",count
 		print "q_count:",q_count
 				
-
+def clean_the_tweet_text(raw_tweet):
+	clean_tweet = raw_tweet.lower().encode("utf-8")
+	regex_form = '^rt\s+|@\w+:*|https?://[\w\.\/]*'
+	clean_tweet = re.sub(regex_form, '', clean_tweet)
+	return clean_tweet
 
 def write_to_db():
 	client, db = connect()
@@ -49,6 +60,4 @@ def write_to_db():
 	print "Done  putting in mongodb" 
 
 if __name__ == "__main__":
-	# write_to_csv()
-
-
+	write_to_csv()
