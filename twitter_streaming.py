832b26896f872b4e4c4a4bcc4cc33b70a28f31d1
moontails <dragontransformer@gmail.com>
2015-05-03 20:01:13 -0500

    changing to stream only questions

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -3,7 +3,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-#Variables that contains the user credentials to access Twitter API 
+#Variables that contains the user credentials to access Twitter API
 access_token = "131866583-DUasEYMwlQevDADf4a8E6KJKrgmpOsrDOp5L3276"
 access_token_secret = "2tZo53FHQ22f3fDzxDgkS4Ai3xTsK16jixlVzxmmvzzMm"
 consumer_key = "JgVBDjLUQV4NEAiHxUFIFhi3r"
@@ -30,4 +30,4 @@ if __name__ == '__main__':
     stream = Stream(auth, l)
 
     #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])
+    stream.filter(track=['?','who', 'what', 'when', 'where', 'why', 'how', 'do', 'is', 'could', 'can', "can't", 'cant', 'would', "wouldn't", "wouldnt", 'should', "shouldn't", "shouldnt", 'did', 'will', 'has', 'have', "won't", 'does', 'wont', 'doesnt', "doesn\'t", 'had', 'are'])
